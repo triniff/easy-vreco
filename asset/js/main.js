dc10e16fdd2e98dbf4d4dbae322599c929f80a48
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-06-19 13:35:35 -0400

    agregando icono bici

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,35 +1,37 @@
 function initMap(){
-	var map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 5,
-		center: {lat: -9.1191427, lng: -77.0349046},
-		mapTypeControl: false,
-		zoomControl: false,
-		streetViewControl: false
-	});
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: {lat: -9.1191427, lng: -77.0349046},
+        mapTypeControl: false,
+        zoomControl: false,
+        streetViewControl: false,
+    });
 
-	function buscar(){
-		if(navigator.geolocation){
-			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-		}
-	}
-	document.getElementById("encuentrame").addEventListener("click", buscar);
-	var latitud,longitud;
+    function buscar(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+        }
+    }
+    document.getElementById("encuentrame").addEventListener("click", buscar);
+    var latitud,longitud;
 
-	var funcionExito = function(posicion){
-		latitud = posicion.coords.latitude;
-		longitud = posicion.coords.longitude;
+    var funcionExito = function(posicion){
+        latitud = posicion.coords.latitude;
+        longitud = posicion.coords.longitude;
 
-		var miUbicacion = new google.maps.Marker({
-			position: {lat:latitud, lng:longitud},
-			animation: google.maps.Animation.DROP,
-			map: map
-		});
+        var miUbicacion = new google.maps.Marker({
+            position: {lat:latitud, lng:longitud},
+            animation: google.maps.Animation.DROP,
+            map: map,
+            icon:"asset/img/bicycle.png"
+        });
 
-		map.setZoom(17);
-		map.setCenter({lat:latitud, lng:longitud});
-	}
+        map.setZoom(17);
+        map.setCenter({lat:latitud, lng:longitud});
+    }
 
-	var funcionError = function(error){
-		alert("Tenemos un problema con encontrar tu ubicación");
-	}
+    var funcionError = function(error){
+        alert("Tenemos un problema con encontrar tu ubicación");
+    }
+
 }
